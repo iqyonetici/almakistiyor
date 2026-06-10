a7fb30901f0e94e8c9c386da0b357dcb23c3e4c1
IQ TEKNO <iqyonetici@gmail.com>
2026-06-10 16:49:21 +0300

    favicon kare a logosu yapildi

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-﻿import '../styles/globals.css'
+import '../styles/globals.css'
 import Head from 'next/head'
 import { AuthProvider } from '../context/AuthContext'
 
@@ -6,9 +6,9 @@ export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Head>
-        <link rel="icon" href="/icon-32.png" sizes="32x32" />
-        <link rel="icon" href="/icon-192.png" sizes="192x192" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="icon" href="/almakistiyor-icon.png" />
+        
+        <link rel="apple-touch-icon" href="/almakistiyor-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#085549" />
       </Head>
