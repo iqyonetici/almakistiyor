0ce7f0cbbbb42be95628ba50a1b4c36eb41aabdf
IQ TEKNO <iqyonetici@gmail.com>
2026-06-12 10:16:41 +0300

    favicon.ico eklendi, cache bust v3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+﻿import '../styles/globals.css'
 import Head from 'next/head'
 import { AuthProvider } from '../context/AuthContext'
 
@@ -6,9 +6,9 @@ export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Head>
-        <link rel="icon" href="/almakistiyor-icon.png" />
-        
-        <link rel="apple-touch-icon" href="/almakistiyor-icon.png" />
+        <link rel="icon" href="/favicon.ico?v=3" sizes="any" />
+        <link rel="icon" type="image/png" href="/icon-32.png?v=3" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#085549" />
       </Head>
